09d5a55c36f68d45a20c88762b087da3fec6b129
sbritotiti <sbritotiti@gmail.com>
2025-05-01 18:36:51 -0400

    main v1

---
+++ Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+        public static void main(String[] args) {
+                String messageTest = "messageTest";
+                System.out.println("Hello: " + messageTest);
+                Scanner s = new Scanner(System.in);
+                System.out.print("Numero 1: ");
+                int n1 = s.nextInt();
+                System.out.print("Numero 2: ");
+                int n2 = s.nextInt();
+                System.out.println("Suma: " + (n1 + n2));
+                System.out.println("Resta: " + (n1 - n2));
+                System.out.println("Divi: " + (n1 * n2));
+                System.out.println("");
+                System.out.println("");
+                System.out.println("------------");
+                s.close();
+        }
+
+}
