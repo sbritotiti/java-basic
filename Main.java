191740fc78ec9252016b2bcf2747e6a75b235fdf
sbritotiti <sbritotiti@gmail.com>
2025-05-02 18:09:07 -0500

    2do dia aprendiendo Java

--- Main.java
+++ Main.java
@@ -21,7 +21,18 @@ public class Main {
                 System.out.println("");
                 System.out.println("");
                 System.out.println("------------");
+                int h = Potencia(n1, n2);
+                System.out.println("La potencia es: " + h);
                 s.close();
+        }
+
+        public static int Potencia (int i, int o){
+                int c = 1;
+                while (o >= 1) {
+                   c = c * i;
+                   o = o-1;     
+                }
+                return c;
         }
 
 }
